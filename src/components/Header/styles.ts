b19edc98ba0e40af33687aa5b6ae0267ec5e0971
João Amadeu <jmamadeu2000@gmail.com>
2020-05-02 06:58:21 +0100

    feat: Initial commit

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 60px;
+  background: ${(props) => props.theme.colors.primary};
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 30px;
+  font-weight: bold;
+  justify-content: space-between;
+`;
---
+++ src/styles/theme/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#333',
+    secondary: '#D63AF9',
+
+    background: '#222',
+    text: '#fff',
+  },
+};
